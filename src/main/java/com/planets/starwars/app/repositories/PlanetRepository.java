efcaba0892be7a2c8fc086af9cdbba94cf0fc5d4
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-05 15:08:10 -0300

    feat: Criar repository da entidade Planet

---
+++ src/main/java/com/planets/starwars/app/repositories/PlanetRepository.java
@@ -0,0 +1,9 @@
+package com.planets.starwars.app.repositories;
+
+import com.planets.starwars.app.models.Planet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet, String> {
+}
