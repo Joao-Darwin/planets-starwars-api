f67427200a213645834689cc5d4c5c85cb41d369
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-06 22:28:16 -0300

    feat: Criar o handler de exceptions no projeto

---
+++ src/main/java/com/planets/starwars/app/exceptions/handler/CustomizeExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.planets.starwars.app.exceptions.handler;
+
+import com.planets.starwars.app.exceptions.ResponseEntityException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+@RestController
+public class CustomizeExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ResponseEntityException> handleAllExceptions(Exception exception, WebRequest webRequest) {
+        ResponseEntityException responseEntityException = new ResponseEntityException(Instant.now(), exception.getMessage(), webRequest.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseEntityException);
+    }
+}
