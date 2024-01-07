3297f7ac8ad63baaf952c7edc22060608f9d83c2
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-06 22:05:43 -0300

    refactor: Criar método para converter PlanetRequestDTO para Planet

---
+++ src/main/java/com/planets/starwars/app/utils/ConvertPlanetRequestDTOToPlanetEntity.java
@@ -0,0 +1,17 @@
+package com.planets.starwars.app.utils;
+
+import com.planets.starwars.app.dto.v1.PlanetRequestDTO;
+import com.planets.starwars.app.models.Planet;
+
+public class ConvertPlanetRequestDTOToPlanetEntity {
+
+    public static Planet convertPlanetRequestDTOToPlanetEntity(PlanetRequestDTO planetRequestDTO) {
+        Planet planet = new Planet();
+
+        planet.setName(planetRequestDTO.getName());
+        planet.setClimate(planetRequestDTO.getClimate());
+        planet.setGround(planetRequestDTO.getGround());
+
+        return planet;
+    }
+}
