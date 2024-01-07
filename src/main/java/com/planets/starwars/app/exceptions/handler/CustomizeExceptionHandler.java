6d327ce697cc31597559b036314a0d28a3e50b2d
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-07 01:22:27 -0300

    feat: Adicionar exception para quando nome do planeta não for válido

--- src/main/java/com/planets/starwars/app/exceptions/handler/CustomizeExceptionHandler.java
+++ src/main/java/com/planets/starwars/app/exceptions/handler/CustomizeExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.planets.starwars.app.exceptions.handler;
 
+import com.planets.starwars.app.exceptions.PlanetNotFindException;
 import com.planets.starwars.app.exceptions.ResponseEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,13 @@ public class CustomizeExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ResponseEntityException> handleAllExceptions(Exception exception, WebRequest webRequest) {
+        ResponseEntityException responseEntityException = new ResponseEntityException(Instant.now(), exception.getMessage(), webRequest.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseEntityException);
+    }
+
+    @ExceptionHandler(PlanetNotFindException.class)
+    public final ResponseEntity<ResponseEntityException> handlePlanetNotFindException(Exception exception, WebRequest webRequest) {
         ResponseEntityException responseEntityException = new ResponseEntityException(Instant.now(), exception.getMessage(), webRequest.getDescription(false));
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseEntityException);
