ebb85be1e43f8217773ef6b2b3d1a2e1711c8d9b
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-06 22:04:44 -0300

    refactor: Criar método de converção de Planet para PlanetResponseDTO

---
+++ src/main/java/com/planets/starwars/app/utils/ConvertPlanetEntityToPlanetResponseDTO.java
@@ -0,0 +1,19 @@
+package com.planets.starwars.app.utils;
+
+import com.planets.starwars.app.dto.v1.PlanetResponseDTO;
+import com.planets.starwars.app.models.Planet;
+
+public class ConvertPlanetEntityToPlanetResponseDTO {
+
+    public static PlanetResponseDTO convertPlanetEntityToPlanetResponseDTO(Planet planetEntity) {
+        PlanetResponseDTO planetResponseDTO = new PlanetResponseDTO();
+
+        planetResponseDTO.setId(planetEntity.getId());
+        planetResponseDTO.setName(planetEntity.getName());
+        planetResponseDTO.setClimate(planetEntity.getClimate());
+        planetResponseDTO.setGround(planetEntity.getGround());
+        planetResponseDTO.setFilmAppearances(planetEntity.getFilmAppearances());
+
+        return planetResponseDTO;
+    }
+}
