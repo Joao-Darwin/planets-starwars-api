578a579e2ea375798779e524121a6e95577cb57e
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-12 15:53:13 -0300

    feat: Criar método utilitário pra converter lista de Planet em uma lista de PlanetResponseDTO

--- src/main/java/com/planets/starwars/app/utils/conversions/ConvertPlanetEntityToPlanetResponseDTO.java
+++ src/main/java/com/planets/starwars/app/utils/conversions/ConvertPlanetEntityToPlanetResponseDTO.java
@@ -3,7 +3,21 @@ package com.planets.starwars.app.utils.conversions;
 import com.planets.starwars.app.dto.v1.PlanetResponseDTO;
 import com.planets.starwars.app.models.Planet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConvertPlanetEntityToPlanetResponseDTO {
+
+    public static List<PlanetResponseDTO> convertListPlanetEntityToListPlanerResponseDTO(List<Planet> planetList) {
+        List<PlanetResponseDTO> planetResponseDTOList = new ArrayList<>();
+
+        for (Planet planet : planetList) {
+            PlanetResponseDTO planetResponseDTO = convertPlanetEntityToPlanetResponseDTO(planet);
+            planetResponseDTOList.add(planetResponseDTO);
+        }
+
+        return planetResponseDTOList;
+    }
 
     public static PlanetResponseDTO convertPlanetEntityToPlanetResponseDTO(Planet planetEntity) {
         PlanetResponseDTO planetResponseDTO = new PlanetResponseDTO();
