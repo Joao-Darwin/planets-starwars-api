01a19078708e6125cc2268b349e681278741b1a8
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-12 15:23:48 -0300

    refactor: Substituir HttpClient por RestTemplate ao fazer requisição pra API externa

--- src/main/java/com/planets/starwars/app/utils/requests/RequestToStarWarsOficialAPI.java
+++ src/main/java/com/planets/starwars/app/utils/requests/RequestToStarWarsOficialAPI.java
@@ -1,40 +1,26 @@
 package com.planets.starwars.app.utils.requests;
 
 import com.planets.starwars.app.exceptions.PlanetNotFindException;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.planets.starwars.app.utils.requests.dtos.PlanetDataAPIOfficialResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class RequestToStarWarsOficialAPI {
     private static final String baseUrl = "https://swapi.dev/api/";
 
-    public static int GetPlanetFilmAppearances(String namePlanet) throws IOException, InterruptedException, PlanetNotFindException {
-        HttpClient client = HttpClient.newHttpClient();
+    public static int GetPlanetFilmAppearances(String namePlanet) throws PlanetNotFindException {
+        RestTemplate restTemplate = new RestTemplate();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(baseUrl + "planets/?search=" + namePlanet))
-                .GET()
-                .build();
+        ResponseEntity<PlanetDataAPIOfficialResponse> planetData = restTemplate.getForEntity(baseUrl + "planets/?search=" + namePlanet, PlanetDataAPIOfficialResponse.class);
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        PlanetDataAPIOfficialResponse planetDataDTO = planetData.getBody();
 
-        if (!response.body().contains("films")) {
-            throw new PlanetNotFindException("Planet not find. Searched name: " + namePlanet);
+        if (planetDataDTO.getCount() == 0) {
+            throw new PlanetNotFindException("Planet don't find. Name searched: " + namePlanet);
         }
 
-        Pattern patternFilms = Pattern.compile("films:\\[(https://swapi.dev/api/films/\\d+/,?)*]");
-        String text = response.body().replaceAll("\"", "");
-        Matcher matcher = patternFilms.matcher(text);
-
-        if (matcher.find()) {
-            text = matcher.group().replaceAll("[(films)\\[\\]]*", "");
-            String[] values = text.split(",");
-            return values.length;
+        if (!planetDataDTO.getResults().isEmpty()) {
+            return planetDataDTO.getResults().get(0).getFilms().size();
         }
 
         throw new RuntimeException("Occurred some problem");
