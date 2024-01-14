032ae3cf941a776e9386dc235c5133a82bd38211
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-14 14:08:40 -0300

    feat: Criar exceção personalizada pra quando o planeta já existir no banco

---
+++ src/main/java/com/planets/starwars/app/exceptions/PlanetAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.planets.starwars.app.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PlanetAlreadyExistsException extends RuntimeException{
+
+    public PlanetAlreadyExistsException(String message) {
+        super(message);
+    }
+}
