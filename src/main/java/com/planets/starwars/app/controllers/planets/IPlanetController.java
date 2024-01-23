cba478d587803900291e5fe4a0920639ae7557ed
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-23 15:46:09 -0300

    feat: Adicionar Tag a interface do PlanetController e annotation de Operation documentando endpoints
    Issue: #1

--- src/main/java/com/planets/starwars/app/controllers/planets/IPlanetController.java
+++ src/main/java/com/planets/starwars/app/controllers/planets/IPlanetController.java
@@ -2,21 +2,98 @@ package com.planets.starwars.app.controllers.planets;
 
 import com.planets.starwars.app.dto.v1.PlanetRequestDTO;
 import com.planets.starwars.app.dto.v1.PlanetResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+@Tag(name = "Planets", description = "Endpoints for managing Planets")
 public interface IPlanetController {
 
+    @Operation(summary = "Adds a new Planet",
+            description = "Adds a new Planet by passing in a JSON e XML!",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PlanetResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<PlanetResponseDTO> create(PlanetRequestDTO planet);
 
+    @Operation(summary = "Find all planets",
+            description = "Find all planets from API on formats JSON e XML!",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PlanetResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<List<PlanetResponseDTO>> findAll();
 
+    @Operation(summary = "Find one planet by name",
+            description = "Find by name planet on formats JSON e XML!",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PlanetResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<PlanetResponseDTO> findByName(String namePlanet);
 
+    @Operation(summary = "Find one planet by id",
+            description = "Find by id planet on formats JSON e XML!",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PlanetResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<PlanetResponseDTO> findById(String id);
 
+    @Operation(summary = "Updates a Planet",
+            description = "Updates a planet on formats JSON e XML!",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PlanetResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<PlanetResponseDTO> update(String id, PlanetRequestDTO planet);
 
+    @Operation(summary = "Delete a planet by id",
+            description = "Delete a planet by id planet",
+            tags = {"Planets"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     ResponseEntity<Void> delete(String id);
 }
