3da2b6f38a979def081936fdef82c09e91db45cb
João Darwin <joaodarwin.ip22@gmail.com>
2024-01-26 14:13:55 -0300

    feat: Criar configuração para suporte ao CORS globalmente no projeto
    Issue: #5

--- src/main/java/com/planets/starwars/app/config/WebConfig.java
+++ src/main/java/com/planets/starwars/app/config/WebConfig.java
@@ -1,12 +1,26 @@
 package com.planets.starwars.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.origins}")
+    private String corsOrigins = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedOrigins(corsOrigins);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
